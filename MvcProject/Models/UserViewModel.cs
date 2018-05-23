d5a43b9352541a386f704085c31408690488ee4d
Andrew <39457364+Amiro30@users.noreply.github.com>
2018-05-23 21:59:59 +0300

    Add files via upload

---
+++ MvcProject/Models/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcProject.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display (Name = "Login")]
+        public string Login { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public byte[] UserPhoto { get; set; }
+
+       
+    }
+}
--- MvcProject/Startup.cs
+++ MvcProject/Startup.cs
@@ -8,7 +8,7 @@ namespace MvcProject
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            
         }
     }
 }
