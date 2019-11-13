f210a4c2ff1ff4bf40060cb372c72f9a1bb5d67e
Andrew Miroshnichenko <amiro30@i.ua>
2019-11-13 09:37:11 +0200

    check for commit- it works

--- DAL/Entities/Photo.cs
+++ DAL/Entities/Photo.cs
@@ -17,14 +17,14 @@ namespace DAL.Entities
         [Required]
         public string Title { get; set; }
 
-        //PhotoFile. This is a picture file
+        //PhotoFile. This is a picture file file
         [DisplayName("Picture")]
         [MaxLength]
         public byte[] PhotoFile { get; set; }
 
         public int TotalRate { get; set; }
 
-        //ImageMimeType, stores the MIME type for the PhotoFile
+        //ImageMimeType, stores the MIME type for the PhotoFile 
         [HiddenInput(DisplayValue = false)]
         public string ImageMimeType { get; set; }
 
